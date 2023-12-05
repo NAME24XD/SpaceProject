eba789f697e669d203d9ce5b7686aec80ed07d52
name_xd <kirim65987@gmail.com>
2023-12-05 19:39:33 +0300

    sp_9_3

--- app.js
+++ app.js
@@ -25,13 +25,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var MongoStore = require('connect-mongo');
 app.use(session({
-  secret: "Space"
-  ,
+  secret: "Space",
   cookie:{maxAge:60*1000},
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true,
+  secure: true,
+  store: MongoStore.create({mongoUrl: 'mongodb://localhost/space'})
   }))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/space', space);
