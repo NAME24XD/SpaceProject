f3065c7fe6141662b710adb52cfe03ef8cdbe70f
name_xd <kirim65987@gmail.com>
2023-12-05 19:54:04 +0300

    sp_9_4

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 const Space = require("../models/space").Space
 
 
-/* GET home page. */
-router.get('/', async (req, res, next) => {
-  try {
-    const menu = await Space.find({}, { _id: 0, title: 1, nick: 1 });
-    req.session.greeting ="Hi!!!"
-    res.render('index', { title:'Space', menu:menu });}
+/* GET home page. */  
+  router.get('/', async(req, res, next) => {
+    try{
+      const menu = await Space.find({}, { _id: 0, title: 1, nick: 1 });
+      res.render('index', { title: 'Space', menu:menu,counter:req.session.counter });
+    }
     catch (err) {next(err);}
 });
 
