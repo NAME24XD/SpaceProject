b621267f597d573fd3683a115ddca4536cdec0e5
name_xd <kirim65987@gmail.com>
2023-12-13 15:16:01 +0300

    sp_11_7

--- middleware/createUser.js
+++ middleware/createUser.js
@@ -1,7 +1,19 @@
-const User = require("./../models/User").User;
+// const User = require("./../models/User").User;
+var db = require('../mySQLConnect.js');
 
 module.exports = async function(req, res, next) {
     res.locals.user = null
+    console.log('Проверяем сессию')
+    console.log(req.session.user)
+    db.query(`SELECT * FROM user WHERE user.id = '${req.session.user}'`, (err, users) => {
+    if(err)
+    return next(err)
+    if(users.length > 0) {
+    res.locals.user = users[0];
+    }
+    next()
+    })
+}
     // try {
     //     const user = await User.findById(req.session.user).exec();
     //         if (!user) {
@@ -12,5 +24,4 @@ module.exports = async function(req, res, next) {
     // } catch (err) {
     //     next(err);
     // }
-    next();
-};
+
