78f0564e9c8300fc20afce18a811f92c786e1746
name_xd <kirim65987@gmail.com>
2023-12-13 16:01:50 +0300

    sp_11_4

--- routes/space.js
+++ routes/space.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../mySQLConnect.js');
 // var Space = require("../models/space").Space
-// const checkAuth = require('../middleware/checkAuth');
+const checkAuth = require('../middleware/checkAuth');
 
 
 router.get("/:nick", function(req, res, next) {
@@ -11,7 +11,7 @@ router.get("/:nick", function(req, res, next) {
     console.log(err);
     if(err) return next(err)
     } else {
-      if(spaces.length == 0) return next(new Error("Такого пышного древа нет"))
+      if(spaces.length == 0) return next(new Error("нет такой планеты "))
         var space = spaces[0];
         res.render('space', {
           title: space.title,
@@ -21,37 +21,6 @@ router.get("/:nick", function(req, res, next) {
 }
 })
 });
-
-router.get('/', function(req, res, next) {
-  res.send('<h1>Это экран для списка планет</h1>');
-  });
-  
-
-router.get("/:nick", function(req, res, next){
-  db.query(`SELECT * FROM space WHERE space.nick = '${req.params.nick}'`, (err, spaces) => {             
-      if(err) {
-          console.log(err);
-          if(err) return next(err)
-      } else {
-          if(spaces.length == 0) return next(new Error("Нет такой планеты во вселенной"))
-          var space = spaces[0];
-          res.render('space', {
-          title   : space.title,
-          picture : space.avatar,
-          about   : space.about
-      })}
-  })
-})
-
-// function renderSpace(res, title, picture, desc, sps) {
-//   console.log(sps);
-
-//   res.render('space', {
-//     title: title,
-//     picture: picture,
-//     desc: desc,
-//   });
-// }
 
 
 module.exports = router;
